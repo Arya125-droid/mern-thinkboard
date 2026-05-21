da867bd5de3a24b72877da7b84428e459ea45f5b
Anshuman <aryaanshuman125@gmail.com>
2026-05-21 15:21:08 +0530

    Full stack notes site

---
+++ backend/src/controllers/notesControllers.js
@@ -0,0 +1,58 @@
+import Note from '../models/Note.js';
+
+export async function getAllNotes (_, res) {
+    try {
+        const notes = await Note.find().sort({ createdAt: -1 });
+        res.status(200).json(notes);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching notes', error: error.message });
+    }
+}
+export async function getNotesById (req, res) {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+        res.status(200).json(note);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching note', error: error.message });
+    }
+}
+export async function createNote(req, res){
+    try {
+        const { title, content } = req.body;
+        console.log("Received note data:", { title, content });
+        const newNote = new Note({
+            title,
+            content
+        });
+        const savedNote = await newNote.save();
+        res.status(201).json({message: 'Note created successfully', note: savedNote});
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating note', error: error.message });
+    }
+}
+export async function updateNote(req, res) {
+    try {
+        const {title, content} = req.body;
+        const newNote = await Note.findByIdAndUpdate(req.params.id, {title, content}, {new: true});
+        if (!newNote) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+        res.status(200).json({ message: 'Note updated successfully', note: newNote });
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating note', error: error.message });
+    }
+}
+export async function deleteNote(req, res){
+    try {
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+        if (!deletedNote) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+        res.status(200).json({ message: 'Note deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting note', error: error.message });
+    }
+}
